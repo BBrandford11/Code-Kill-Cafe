c13089da91d9c469ffcb3501ef3367c6b82bb03d
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-03-13 13:27:20 -0600

    Co-authored-by: Bryce Brandford <BBrandford11@users.noreply.github.com> Co-authored-by: Adetola Fadairo <Fadymain@users.noreply.github.com> styling changes, add todo list

--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -15,22 +15,32 @@ $(document).ready(function(){
 
   $("#btn3").on("click", function (e) {
     console.log("button 3 clicked");
+    const $li = $('<li>').text("OpenSource Sandwich")
+    $(".order-list").append($li)
   });
 
   $("#btn4").on("click", function (e) {
     console.log("button 4 clicked");
+    const $li = $('<li>').text("Fri-Data")
+    $(".order-list").append($li)
   });
 
   $("#btn5").on("click", function (e) {
     console.log("button 5 clicked");
+    const $li = $('<li>').text("Camel Case-role")
+    $(".order-list").append($li)
   });
 
   $("#btn6").on("click", function (e) {
     console.log("button 6 clicked");
+    const $li = $('<li>').text("Arctic VARCHAR")
+    $(".order-list").append($li)
   });
 
   $("#btn7").on("click", function (e) {
     console.log("button 7 clicked");
+    const $li = $('<li>').text("Cookies")
+    $(".order-list").append($li)
   });
 
 
