2e8281691f6f0eebb4d0c8baa4af4e40f5160535
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-03-15 13:22:00 -0600

    Co-authored-by: Bryce Brandford <BBrandford11@users.noreply.github.com> Co-authored-by: Adetola Fadairo <Fadymain@users.noreply.github.com> changes to main page and order buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,32 +1,34 @@
-// Client facing scripts here
-var slideIndex = 1;
-showSlides(slideIndex);
+  let slideIndex = 1;
+  setTimeout(function(){
+    showSlides(slideIndex);
+  }, 30);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
+    }
+    for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
 
 
 
--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -1,50 +1,64 @@
 $(document).ready(function(){
+
   const $btn1 = $("#btn1")
+  let total = 0
 
   $("#btn1").on("click", function (e) {
     console.log("button 1 clicked");
-    const $li = `<div class="order1"> <p>FullStack of pancakes</p> <p>🥞</p> <p>cost</p> </div>`
+    const $li = `<div class="order1"> <p>FullStack of pancakes</p> <p>🥞</p> <p>$4.99</p> </div>`
     $(".order-list").append($li)
+    total += 4.99;
+    $(".total").text(`$${total}`)
   });
 
 
   $("#btn2").on("click", function (e) {
     console.log("button 2 clicked");
-    const $li = `<div class="order1"> <p>API platter</p> <p>🍽️</p> <p>cost</p> </div>`
+    const $li = `<div class="order1"> <p>API platter</p> <p>🍽️</p> <p>$6.50</p> </div>`
     $(".order-list").append($li)
+    total += 6.50;
+    $(".total").text(`$${total}`)
   });
 
   $("#btn3").on("click", function (e) {
     console.log("button 3 clicked");
-    const $li = `<div class="order1"> <p>Opensource Sandwich</p> <p>🥪</p> <p>cost</p> </div>`
+    const $li = `<div class="order1"> <p>Opensource Sandwich</p> <p>🥪</p> <p>$7.87</p> </div>`
     $(".order-list").append($li)
+    total += 7.87;
+    $(".total").text(`$${total}`)
   });
 
   $("#btn4").on("click", function (e) {
     console.log("button 4 clicked");
-    const $li = `<div class="order1"> <p>Fri-Data</p> <p>🍟</p> <p>cost</p> </div>`
+    const $li = `<div class="order1"> <p>Fri-Data</p> <p>🍟</p> <p>$5.00</p> </div>`
     $(".order-list").append($li)
+    total += 5.00;
+    $(".total").text(`$${total}`)
   });
 
   $("#btn5").on("click", function (e) {
     console.log("button 5 clicked");
-    const $li = `<div class="order1"> <p>Camel Case-role</p> <p>🍞</p> <p>cost</p> </div>`
+    const $li = `<div class="order1"> <p>Camel Case-role</p> <p>🍞</p> <p>$8.50</p> </div>`
     $(".order-list").append($li)
+    total += 8.50;
+    $(".total").text(`$${total}`)
   });
 
   $("#btn6").on("click", function (e) {
     console.log("button 6 clicked");
-    const $li = `<div class="order1"> <p>Arctic VARCHAR</p> <p>🥬</p> <p>cost</p> </div>`
+    const $li = `<div class="order1"> <p>Arctic VARCHAR</p> <p>🥬</p> <p>$2.25</p> </div>`
     $(".order-list").append($li)
+    total += 2.25;
+    $(".total").text(`$${total}`)
   });
 
   $("#btn7").on("click", function (e) {
     console.log("button 7 clicked");
-    const $li = `<div class="order1"> <p>Arctic Cookies</p> <p>🍪</p> <p>cost</p> </div>`
+    const $li = `<div class="order1"> <p>Arctic Cookies</p> <p>🍪</p> <p>$3.14</p> </div>`
     $(".order-list").append($li)
+    total += 3.14;
+    $(".total").text(`$${total}`)
   });
-
-
 
 });
 
