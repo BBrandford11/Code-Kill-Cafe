6a41fd63aa4bedfd177ec64fe18faf34b8ebb136
BBrandford11 <brandfordbryce@gmail.com>
2022-03-16 16:30:44 -0600

    ads token .env

--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -1,60 +1,66 @@
-
-$(document).ready(function(){
+$(document).ready(function () {
   let total = 0;
   let newTotal = 0;
-  const $btn1 = $("#btn1")
-  const buttonsArray = ["#btn1", "#btn2", "#btn3", "#btn4", "#btn5", "#btn6", "#btn7"];
-  const content = ["FullStack of pancakes", "API platter", "Opensource Sandwich", "Fri-Data", "Camel Case-role", "Arctic VARCHAR", "Arctic Cookies"]
-  const cost = [4.99, 6.50, 7.87, 5.00, 8.50, 2.25, 3.14]
-  const emojis = ["🥞", "🍽️", "🥪", "🍟", "🍞", "🥬", "🍪"]
+  const $btn1 = $("#btn1");
+  const buttonsArray = [
+    "#btn1",
+    "#btn2",
+    "#btn3",
+    "#btn4",
+    "#btn5",
+    "#btn6",
+    "#btn7",
+  ];
+  const content = [
+    "FullStack of pancakes",
+    "API platter",
+    "Opensource Sandwich",
+    "Fri-Data",
+    "Camel Case-role",
+    "Arctic VARCHAR",
+    "Arctic Cookies",
+  ];
+  const cost = [4.99, 6.5, 7.87, 5.0, 8.5, 2.25, 3.14];
+  const emojis = ["🥞", "🍽️", "🥪", "🍟", "🍞", "🥬", "🍪"];
   for (let i = 0; i < buttonsArray.length; i++) {
     $(buttonsArray[i]).on("click", function (e) {
       const idNumber = i + 1;
-      const $li = `<div class="order1" id="${'order-' + idNumber}"> <p>${content[i]}</p> <p>${emojis[i]}</p> <p>$${cost[i].toFixed(2)}</p> <button class="button-31">Delete</button> </div>`
-      $(".order-list").append($li)
+      const $li = `<div class="order1" id="${"order-" + idNumber}"> <p>${
+        content[i]
+      }</p> <p>${emojis[i]}</p> <p>$${cost[i].toFixed(
+        2
+      )}</p> <button class="button-31">Delete</button> </div>`;
+      $(".order-list").append($li);
       total += cost[i];
       // total = total.toFixed(2);
-      $(".total").text(`$${total.toFixed(2)}`)
+      $(".total").text(`$${total.toFixed(2)}`);
 
       $(".order1").on("click", function (e) {
-        e.stopImmediatePropagation()
-        $(this).remove()
+        e.stopImmediatePropagation();
+        $(this).remove();
         newTotal = total - cost[i];
         total = newTotal;
-        $(".total").text(`$${total.toFixed(2)}`)
-      })
+        $(".total").text(`$${total.toFixed(2)}`);
+      });
     });
   }
 
-
-
-  $('.place-order').on("click", function (e) {
-    console.log("button clicked")
-    sendMessage()
-
-
+  $(".place-order").on("click", function (e) {
+    console.log("button clicked");
+    sendMessage();
   });
 
-  function sendMessage () {
+  function sendMessage() {
     $.ajax({
       type: "POST",
       url: "/menu/message",
       data: {
-        body: 'hello',
-        messagingServiceSid: 'MGe5d4ed39a658a6df660126841e26a05f',
-        to: '+14039934171'
-
+        body: "hello",
+        messagingServiceSid: "MGe5d4ed39a658a6df660126841e26a05f",
+        to: "+14039934171",
       },
-
     })
-    .then((result) => {
-
-    }).catch((err) => {
-
-    });
-
+      .then((result) => {})
+      .catch((err) => {});
   }
-
-
 });
-
--- routes/menu.js
+++ routes/menu.js
@@ -4,39 +4,31 @@
  *   these routes are mounted onto /users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-const express = require('express');
-const router  = express.Router();
-const client = require('twilio')('AC5afee5d9e3ca0c72f8822021269a9f71', '36ba2eb2298bdcc630e7f324a28481ad');
+const client = require('twilio')(accountSid, authToken);
+
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    res.render("menu")
-  //   db.query(`SELECT * FROM users;`)
-  //     .then(data => {
-  //       const users = data.rows;
-  //       res.json({ users });
-  //     })
-  //     .catch(err => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-   });
-   router.post("/message",(req,res) => {
-     const{ body, messagingServiceSid, to  } = req.body;
-     console.log(req.body)
+    res.render("menu");
+  });
+  router.post("/message", (req, res) => {
+    const { body, messagingServiceSid, to } = req.body;
+    console.log(req.body);
 
     client.messages
-    .create({
-      body,
-      messagingServiceSid,
-      to
-     })
-    .then(message => console.log(message.sid))
-    .done();
-    return res.json({response: 'message successful'})
-
-   });
+      .create({
+        body,
+        messagingServiceSid,
+        to,
+      })
+      .then((message) => console.log(message.sid))
+      .done();
+    return res.json({ response: "message successful" });
+  });
   return router;
 };
